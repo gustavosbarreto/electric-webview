204e3f9a6f9ff9301abbb619249721f5e4ce71cf
Luis Gustavo S. Barreto <gustavosbarreto@gmail.com>
2016-07-05 15:46:56 -0300

    add httpserver script
    Signed-off-by: Luis Gustavo S. Barreto <gustavosbarreto@gmail.com>

---
+++ scripts/httpserver.py
@@ -0,0 +1,213 @@
+#!/usr/bin/env python2
+
+import BaseHTTPServer
+import StringIO
+import urlparse
+import socket
+import SocketServer
+import re
+import argparse
+
+from instantwebview import WebView
+
+events = [
+    'url_changed',
+    'title_changed',
+    'load_started',
+    'load_finished',
+    'info_message_raised',
+    'warning_message_raised',
+    'error_message_raised',
+    'user_activity'
+]
+
+parser = argparse.ArgumentParser(description='Instant WebView HTTP Interface')
+parser.add_argument('-t', '--transport', type=str, required=True, help='Transport layer')
+parser.add_argument('-p', '--port', type=int, required=True, help='Port to listen')
+args = parser.parse_args()
+
+class CommandsHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+    def send_reply(self, data = ''):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(data if data is not None else '')
+
+    def send_error(self, error, status=500):
+        self.send_response(status)
+        self.end_headers()
+        self.wfile.write(error)
+
+    def do_GET(self):
+        url = urlparse.urlparse(self.path)
+        query = urlparse.parse_qs(url.query)
+        webview = WebView(args.transport)
+
+        if url.path == '/load':
+            url_param = query.get('url', [''])[0]
+
+            if not url_param:
+                self.send_error('missing url param')
+            else:
+                webview.load(url_param)
+                self.send_reply()
+
+            return
+
+        if url.path == '/stop':
+            webview.stop()
+            self.send_reply()
+            return
+
+        if url.path == '/reload':
+            webview.reload()
+            self.send_reply()
+            return
+
+        if url.path == '/back':
+            webview.back()
+            self.send_reply()
+            return
+
+        if url.path == '/forward':
+            webview.forward()
+            self.send_reply()
+            return
+
+        if url.path == '/open':
+            state_param = query.get('state', [''])[0]
+
+            if not state_param:
+                self.send_error('missing state param')
+            else:
+                webview.open(state_param)
+                self.send_reply()
+
+            return
+
+        if url.path == '/close':
+            webview.close()
+            self.send_reply()
+            return
+
+        if url.path == '/current_url':
+            reply = webview.current_url()
+            self.send_reply(reply)
+            return
+
+        if url.path == '/current_title':
+            reply = webview.current_title()
+            self.send_reply(reply)
+            return
+
+        if url.path == '/screenshot':
+            region_param = query.get('region', [''])[0]
+            reply = webview.screenshot(region_param)
+            self.send_reply(reply)
+
+        if url.path == '/get_html':
+            format_param = query.get('format', [''])[0]
+
+            if not format_param:
+                self.send_error('missing format param')
+            else:
+                reply = webview.get_html(format_param)
+                self.send_reply(reply)
+
+            return
+
+        if url.path == '/set_html':
+            file_param = query.get('file', [''])[0]
+
+            if not file_param:
+                self.send_error('missing file param')
+            else:
+                webview.set_html('file', file_param)
+                self.send_reply()
+
+            return
+
+        if url.path == '/exec_js':
+            file_param = query.get('file', [''])[0]
+
+            if not file_param:
+                self.send_error('missing file param')
+            else:
+                reply = webview.exec_js('file', file_param)
+                self.send_reply(reply)
+
+            return
+
+        if url.path == '/subscribe':
+            event_param = query.get('event', [''])[0]
+
+            if not event_param:
+                self.send_error('missing event param')
+            else:
+                reply = webview.subscribe(event_param)
+                reply = re.sub(r'^%s\s' % event_param, '', reply)
+                self.send_reply(reply)
+
+            return
+
+        if url.path == '/idle_time':
+            reply = webview.idle_time()
+            self.send_reply(reply)
+            return
+
+        if url.path == '/block_user_activity':
+            block_param = query.get('block', [''])[0]
+
+            if not block_param:
+                self.send_error('missing block param')
+            else:
+                webview.block_user_activity(block_param)
+                self.send_reply()
+
+            return
+
+        if url.path == '/exec_cmd':
+            cmd_param = query.get('cmd', [''])[0]
+            execution_param = query.get('execution', [''])[0]
+            args_param = query.get('args', [''])[0]
+
+            if not cmd_param:
+                self.send_error('missing cmd param')
+                return
+
+            if not execution_param:
+                self.send_error('missing execution param')
+                return
+
+            reply = webview.exec_cmd(execution_param, cmd_param, args=args_param.split())
+            self.send_reply(reply)
+
+    def do_POST(self):
+        url = urlparse.urlparse(self.path)
+        webview = WebView(args.transport)
+        content_length = self.headers.get('Content-Length')
+
+        if url.path == '/set_html':
+            if not content_length:
+                self.send_reply()
+                return
+
+            data = self.rfile.read(int(content_length))
+            webview.set_html('string', data)
+            self.send_reply()
+            return
+
+        if url.path == '/exec_js':
+            if not content_length:
+                self.send_reply()
+                return
+
+            data = self.rfile.read(int(content_length))
+            webview.exec_js('string', data)
+            self.send_reply()
+            return
+
+class ThreadedHTTPServer(SocketServer.ThreadingMixIn, BaseHTTPServer.HTTPServer):
+    pass
+
+httpd = ThreadedHTTPServer(('0.0.0.0', args.port), CommandsHandler)
+httpd.serve_forever()
---
+++ scripts/instantwebview.py
@@ -0,0 +1,88 @@
+import socket
+import StringIO
+import urllib
+import re
+import sys
+
+class WebView:
+    def __init__(self, transport):
+        self.transport = transport
+        self.transport_type = transport.split(':')[0]
+        self.transport_options = transport.split(':')[1:]
+
+    def send_command(self, command, params = [], wait_for_reply=True):
+        webview = None
+
+        if self.transport_type == 'unixsocket':
+            socket_file = self.transport_options[0]
+            webview = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+            webview.connect(socket_file)
+        elif self.transport_type == 'tcp':
+            address = self.transport_options[0]
+            port = self.transport_options[1]
+            webview = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            webview.connect((address, int(port)))
+
+        webview.send('%s %s\n' % (command, ' '.join(params)))
+
+        if wait_for_reply:
+            buffer = StringIO.StringIO()
+            while True:
+                data = webview.recv(1024)
+                buffer.write(data)
+                if '\n' in data: break
+            return re.sub(r'^%s\s' % command, '', urllib.unquote(buffer.getvalue().splitlines()[0]))
+
+    def load(self, url):
+        self.send_command('load', [url], wait_for_reply=False)
+
+    def stop(self):
+        self.send_command('stop', wait_for_reply=False)
+
+    def reload(self):
+        self.send_command('reload', wait_for_reply=False)
+
+    def back(self):
+        self.send_command('back', wait_for_reply=False)
+
+    def forward(self):
+        self.send_command('forward', wait_for_reply=False)
+
+    def open(self, state):
+        self.send_command('open', [state], wait_for_reply=False)
+
+    def close(self):
+        self.send_command('close', wait_for_reply=False)
+
+    def current_url(self):
+        return self.send_command('current_url')
+
+    def current_title(self):
+        return self.send_command('current_title')
+
+    def screenshot(self, region = ''):
+        return self.send_command('screenshot', [region])
+
+    def set_html(self, source, value):
+        self.send_command('set_html', [source, value], wait_for_reply=False)
+
+    def get_html(self, format):
+        return self.send_command('get_html', [format])
+
+    def exec_js(self, source, value):
+        return self.send_command('exec_js', [source, value])
+
+    def subscribe(self, event):
+        return self.send_command('subscribe', [event])
+
+    def idle_time(self):
+        return self.send_command('idle_time')
+
+    def block_user_activity(self, block):
+        self.send_command('block_user_activity', [block], wait_for_reply=False)
+
+    def exec_cmd(self, execution, command, args=[]):
+        reply = self.send_command('exec_cmd', ['sync' if execution else 'async', command] + args, wait_for_reply=execution == 'sync')
+
+        if reply is not None:
+            return reply
