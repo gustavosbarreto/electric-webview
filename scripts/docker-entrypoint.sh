880de2fdfecb74139298e94af4ed6689c30572df
Luis Gustavo S. Barreto <gustavosbarreto@gmail.com>
2016-07-06 16:01:03 -0300

    docker image: add support for other transport layers
    Signed-off-by: Luis Gustavo S. Barreto <gustavosbarreto@gmail.com>

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -1,11 +1,22 @@
-#!/bin/sh
+#!/bin/bash
 
 [ -z "$RESOLUTION" ] && RESOLUTION=1024x768
 
-xvfb-run -a -s "-screen 0 ${RESOLUTION}x16" /usr/local/bin/instant-webview $* &
+TRANSPORT=$*
+TRANSPORT_NAME=${TRANSPORT/%:*}
 
-INSTANT_WEBVIEW_PID=$!
+echo "unixsocket tcp websocket http" | grep -q $TRANSPORT_NAME || (echo "Invalid transport layer" && exit 1) || exit 1
 
-/usr/src/instant-webview/scripts/httpserver.py $* -p 80 &
+if [ "$TRANSPORT_NAME" = "http" ]; then
+    xvfb-run -a -s "-screen 0 ${RESOLUTION}x16" /usr/local/bin/instant-webview -t unixsocket:/tmp/instant-webview &
+    INSTANT_WEBVIEW_PID=$!
+
+    /usr/src/instant-webview/scripts/httpserver.py -t unixsocket:/tmp/instant-webview -p 80 &
+
+    echo "http: Listening on port 80"
+else
+    xvfb-run -a -s "-screen 0 ${RESOLUTION}x16" /usr/local/bin/instant-webview -t $TRANSPORT &
+    INSTANT_WEBVIEW_PID=$!
+fi
 
 wait $INSTANT_WEBVIEW_PID
